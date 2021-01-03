2d40a5f494cdb143ddebcf22d5c8ba6ac1659748
bilal-fazlani <bilal.m.fazlani@gmail.com>
2021-01-03 14:11:02 +0530

    image sample

--- myplugin/src/main/scala/com/bilalfazlani/myplugin/SimplePlugin.scala
+++ myplugin/src/main/scala/com/bilalfazlani/myplugin/SimplePlugin.scala
@@ -3,14 +3,19 @@ package com.bilalfazlani.myplugin
 import com.bilalfazlani.swiftbar4s.SwiftBarApp
 import com.bilalfazlani.swiftbar4s.dsl._
 
-// object Main extends App {
-//   val bytes = ???
-//   val base64 = java.util.Base64.getEncoder.encodeToString(bytes)
-//   println(base64)
-// }
 
 object SimplePlugin extends SwiftBarApp with MenuDsl {
+
+ def getImage = {
+  val bytes = getClass.getResourceAsStream("/success.png").readAllBytes
+  val base64 = java.util.Base64.getEncoder.encodeToString(bytes)
+  base64
+}
+
   override val appMenu = menu("my-plugin") {
+
+    text("success", image = getImage)
+
     runtime.foreach { r =>
       if (r.osVersion < OSVersion.BigSur)
         text(s"your os version is: ${r.osVersion}")
