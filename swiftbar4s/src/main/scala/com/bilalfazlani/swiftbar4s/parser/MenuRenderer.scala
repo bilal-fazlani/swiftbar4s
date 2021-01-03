0f3d562ce66baa17ae0a239fa76387c0cac16c7d
bilal-fazlani <bilal.m.fazlani@gmail.com>
2021-01-03 20:39:11 +0530

    add plugin info

--- swiftbar4s/src/main/scala/com/bilalfazlani/swiftbar4s/parser/MenuRenderer.scala
+++ swiftbar4s/src/main/scala/com/bilalfazlani/swiftbar4s/parser/MenuRenderer.scala
@@ -3,10 +3,11 @@ package com.bilalfazlani.swiftbar4s.parser
 import com.bilalfazlani.swiftbar4s.dsl._
 
 class MenuRenderer(parser: Parser) {
-    def renderMenu(menuBuilder: MenuBuilder, streaming:Boolean) = 
+    def renderMenu(menuBuilder: MenuBuilder, streaming:Boolean) = {
         if(streaming) println("~~~")
         parser.parse(menuBuilder.build)
           .lines
           .foreach(println)
         menuBuilder.build
+    }
 }
