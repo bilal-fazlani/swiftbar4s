d97913a54052029bd5697dd31c193ed3d5033859
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2023-04-09 11:04:14 +0530

    update versions

--- project/Libs.scala
+++ project/Libs.scala
@@ -6,7 +6,7 @@ object Libs {
 }
 
 object NonProdLibs {
-  lazy val munit = "org.scalameta" %% "munit" % "0.7.29"
+  lazy val munit = "org.scalameta" %% "munit" % "1.0.0-M7"
   lazy val `entities-client` =
     "com.github.bilal-fazlani.akka-http-streaming-events" % "streaming-client_2.13" % "main-SNAPSHOT"
   lazy val rainbowcli = "com.github.bilal-fazlani" % "rainbowcli" % "2.0.1"
