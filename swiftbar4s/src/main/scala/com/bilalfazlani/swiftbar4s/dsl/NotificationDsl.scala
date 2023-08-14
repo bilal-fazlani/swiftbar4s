41bc56dbd16bbfccc275a14f88e1f9b820ca01ec
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2023-08-14 14:57:49 +0530

    add support for href in notifications
    + silent notifications

--- swiftbar4s/src/main/scala/com/bilalfazlani/swiftbar4s/dsl/NotificationDsl.scala
+++ swiftbar4s/src/main/scala/com/bilalfazlani/swiftbar4s/dsl/NotificationDsl.scala
@@ -3,17 +3,20 @@ package com.bilalfazlani.swiftbar4s.dsl
 import scala.sys.process.*
 
 trait NotificationDsl { this: Environment =>
-  //todo: support href
   def notify(
       title: String,
       subtitle: Option[String] = None,
-      body: Option[String] = None
+      body: Option[String] = None,
+      href: Option[String] = None,
+      silent: Boolean = false
   ): Unit = {
     runtime.foreach { r =>
       val baseUrl = s"swiftbar://notify?plugin=${r.pluginFileName}&title=$title"
       val sub     = subtitle.map(x => s"&subtitle=$x").getOrElse("")
       val b       = body.map(x => s"&body=$x").getOrElse("")
-      val url     = baseUrl + sub + b
+      val link    = href.map(x => s"&href=$x").getOrElse("")
+      val s       = if silent then "&silent=true" else ""
+      val url     = baseUrl + sub + b + link + s
       s"""open "$url"""".!!
     }
   }
